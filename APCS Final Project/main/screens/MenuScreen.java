c35349c6fd885d86e453bf75d050487292f301f2
jeffreylee <jeffreylee@jeffreys-mbp.hsd1.ca.comcast.net>
2020-05-16 04:01:48 -0700

    Beginning to implement screen switching and infinite levels

---
+++ APCS Final Project/main/screens/MenuScreen.java
@@ -0,0 +1,48 @@
+package screens;
+
+import gui.Images;
+import main.Game;
+
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+public class MenuScreen extends Screen
+{
+    //private boolean gameToStart = false;
+    public MenuScreen()
+    {
+      //TODO: change later
+        JButton button = new JButton( "Start Game" );
+        button.setBounds( 100, 100, 75, 100 );
+        this.add( button );
+        ImageIcon background = new ImageIcon(
+            this.getClass().getResource( "MenuBackgroundAPCS.gif" ) );
+        background.setImage( background.getImage()
+            .getScaledInstance( 1744, 1000, Image.SCALE_DEFAULT ) );
+        this.getContentPane().add( ( new JLabel( background ) ) );
+        
+        
+        button.addActionListener( ( new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed( ActionEvent e )
+            {
+                
+            }
+        } ) );
+        this.pack();
+    }
+    
+    public boolean startGame()
+    {
+        
+    }
+
+}
---
+++ APCS Final Project/main/screens/Screen.java
@@ -0,0 +1,11 @@
+package screens;
+import java.awt.*;
+import javax.swing.JFrame;
+import gui.Images;
+public abstract class Screen extends JFrame
+{
+    public Screen()
+    {
+        //TODO: Need Object-Oriented Implementation
+    }
+}
