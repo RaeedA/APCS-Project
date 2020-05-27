c74e7637cd2e1b2e2e0e66711d9c51881c77aa2f
jeffreylee <jeffreylee@jeffreys-mbp>
2020-05-26 22:32:13 -0700

    Starting javadocs

--- APCS Final Project/main/gui/Level.java
+++ APCS Final Project/main/gui/Level.java
@@ -13,127 +13,214 @@ import entities.Enemy;
 import entities.User;
 import main.Launcher;
 
+
+/**
+ * Level class which represents the level in which the user will be playing on.
+ * The level contains all the necessary processes, objects, and functions to
+ * start, play, and end the game
+ *
+ * @author Jeffrey Lee, Raeed Azom
+ * @version May 26, 2020
+ * @author Period: 1
+ * @author Assignment: APCS Final Project
+ *
+ * @author Sources: none
+ */
 @SuppressWarnings("serial")
 public class Level extends JFrame implements KeyListener
-{    
+{
     private Room activeRoom;
+
     private Room previousRoom;
+
     private User user;
+
     private Container pane;
+
     private GridBagConstraints constraints;
+
     private long userLevelScore;
+
     private Top top;
-    
-    
-    public Level(String text)
+
+
+    /**
+     * Constructor: sets up the level to prepare for the user to play. prepares
+     * the key listener and initializes the user score
+     * 
+     * @param text
+     */
+    public Level( String text )
     {
-        super(text);
+        super( text );
         addKeyListener( this );
         userLevelScore = 0;
         setup();
     }
-    
+
+
+    /**
+     * Sets up and prepares the level to be displayed
+     */
     public void setup()
     {
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         pane = getContentPane();
-        pane.setLayout(new GridBagLayout());
+        pane.setLayout( new GridBagLayout() );
         constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.BOTH;
     }
-    
-    public Room generateRoom(Room room, boolean isFirst, int roomNum)
+
+
+    /**
+     * Generates and prepares a given room (additionally with enemies), further
+     * specified by its number and whether it is the first room. Additionally
+     * removes the previously generated room (if such a room exists)
+     * 
+     * @param room
+     *            - the Room object that will be generated
+     * @param isFirst
+     *            - true if the room is the first room, false if otherwise
+     * @param roomNum
+     *            - the specified room number
+     * @return Room room - the newly generated room
+     */
+    public Room generateRoom( Room room, boolean isFirst, int roomNum )
     {
-        if(!isFirst)
+        if ( !isFirst )
         {
             previousRoom = activeRoom;
-            closeRoom(previousRoom);
+            closeRoom( previousRoom );
         }
         constraints.gridx = 0;
         constraints.gridy = 1;
         room.setRoomNum( roomNum );
-        pane.add(room, constraints);
+        pane.add( room, constraints );
         activeRoom = room;
-        for(int i = 0; i < Math.random() * 10 + 1; i++)
+        for ( int i = 0; i < Math.random() * 10 + 1; i++ )
         {
-            (new Enemy(room, new Point((int) (Math.random() * 13) + 1, (int) (Math.random() * 13) + 1))).start();
+            ( new Enemy( room,
+                new Point( (int)( Math.random() * 13 ) + 1,
+                    (int)( Math.random() * 13 ) + 1 ) ) ).start();
         }
         constraints.gridx = 0;
         constraints.gridy = 0;
-        top = new Top(room.getLength());
+        top = new Top( room.getLength() );
         top.setRoomNum( roomNum );
         pane.add( top, constraints );
         return room;
     }
-    
+
+
+    /**
+     * Starts the game by generating a room and adding the user to the room
+     */
     public void startGame()
     {
-        Room newRoom = new Room(15);
-        generateRoom(newRoom, true, 1);
-        user = new User(newRoom, new Point(8, newRoom.getMap().getLayout()[0].length - 1));
+        Room newRoom = new Room( 15 );
+        generateRoom( newRoom, true, 1 );
+        user = new User( newRoom,
+            new Point( 8, newRoom.getMap().getLayout()[0].length - 1 ) );
         user.start();
         pack();
-        setMinimumSize(getSize());
-        setSize(getSize().width+30, getSize().height+30);
+        setMinimumSize( getSize() );
+        setSize( getSize().width + 30, getSize().height + 30 );
     }
-    
+
+
+    /**
+     * Ends the game by closing all of the rooms and taking the score to store
+     * into the list of scores in the Game class
+     * 
+     * @param userScore
+     *            - the given score that the user earns
+     */
     public void endGame( long userScore )
     {
-        if(previousRoom != null)
+        if ( previousRoom != null )
         {
-            closeRoom(previousRoom);
+            closeRoom( previousRoom );
         }
-        closeRoom(activeRoom);
+        closeRoom( activeRoom );
         userLevelScore = userScore;
         Launcher.getGame().endGame();
     }
-    
+
+
+    /**
+     * Clears all of the enemies in the given room before removing the room
+     * 
+     * @param room
+     *            - the room to be removed
+     */
     public void closeRoom( Room room )
     {
         room.clearEnemies();
         pane.remove( room );
     }
-    
+
+
+    /**
+     * Accesor method for the user's score: gets the userLevelScore by returning
+     * it
+     * 
+     * @return long userLevelScore - the user's saved score
+     */
     public long getUserLevelScore()
     {
         return userLevelScore;
     }
-    
+
+
+    /**
+     * Method is not used in our program, only specified and declared
+     * 
+     * @param e
+     *            - inputed key event
+     */
     @Override
     public void keyTyped( KeyEvent e )
     {
     }
 
+
+    /**
+     * Receives user input from the keyboard and has the User game character
+     * respond accordingly to the key event
+     * 
+     * @param e
+     *            - inputed key event
+     */
     @Override
     public void keyPressed( KeyEvent e )
     {
         int key = e.getKeyCode();
-        switch(key)
+        switch ( key )
         {
             case KeyEvent.VK_UP:
                 user.setMoving( true );
-                user.setDy(-1);
+                user.setDy( -1 );
                 user.faceUp();
                 break;
             case KeyEvent.VK_DOWN:
                 user.setMoving( true );
-                user.setDy(1);
+                user.setDy( 1 );
                 user.faceDown();
                 break;
             case KeyEvent.VK_RIGHT:
                 user.setMoving( true );
-                user.setDx(1);
+                user.setDx( 1 );
                 user.faceRight();
                 break;
             case KeyEvent.VK_LEFT:
                 user.setMoving( true );
-                user.setDx(-1);
+                user.setDx( -1 );
                 user.faceLeft();
                 break;
             case KeyEvent.VK_Q:
                 break;
             case KeyEvent.VK_O:
-                Enemy ene = new Enemy(activeRoom, new Point(1,1));
+                Enemy ene = new Enemy( activeRoom, new Point( 1, 1 ) );
                 ene.start();
                 break;
             case KeyEvent.VK_X:
@@ -142,41 +229,45 @@ public class Level extends JFrame implements KeyListener
             default:
                 break;
         }
-        
+
     }
 
+
+    /**
+     * Receives user input from the keyboard and has the User game character
+     * respond accordingly to the key event
+     * 
+     * @param e
+     *            - inputed key event
+     */
     @Override
     public void keyReleased( KeyEvent e )
     {
         int key = e.getKeyCode();
-        switch(key)
+        switch ( key )
         {
             case KeyEvent.VK_UP:
                 user.setMoving( false );
-                user.setDy(0);
+                user.setDy( 0 );
                 break;
             case KeyEvent.VK_DOWN:
                 user.setMoving( false );
-                user.setDy(0);
+                user.setDy( 0 );
                 break;
             case KeyEvent.VK_RIGHT:
                 user.setMoving( false );
-                user.setDx(0);
+                user.setDx( 0 );
                 break;
             case KeyEvent.VK_LEFT:
                 user.setMoving( false );
-                user.setDx(0);
+                user.setDx( 0 );
                 break;
             case KeyEvent.VK_Q:
-                user.setAttacking( true ) ;
+                user.setAttacking( true );
                 break;
             default:
                 break;
         }
-        
+
     }
-    
-    
-    
-    
 }
--- APCS Final Project/main/screens/MenuScreen.java
+++ APCS Final Project/main/screens/MenuScreen.java
@@ -14,45 +14,82 @@ import javax.swing.JLabel;
 import gui.Images;
 
 
+/**
+ * MenuScreen class which represents the menu screen that will be displayed when
+ * the game is launched.. The menu screen will consist of a label containing the
+ * background image, a text that displays the title, and a button that, upon
+ * being clicked, will start the level
+ *
+ * @author Jeffrey Lee, Raeed Azom
+ * @version May 26, 2020
+ * @author Period: 1
+ * @author Assignment: APCS Final Project
+ *
+ * @author Sources: none
+ */
 @SuppressWarnings("serial")
 public class MenuScreen extends Screen
 {
+    /**
+     * Constructor: sets up the menu screen with a label containing the
+     * background image, title, and start button. Once the start button is
+     * clicked, screen switches to the game level
+     */
     public MenuScreen()
     {
         super();
-        
-        
-        JButton button = makeButton("Start Game", Images.getButton(), 400, 325, 120, 50, 14);
+
+        JButton button = makeButton( "Start Game",
+            Images.getButton(),
+            400,
+            325,
+            120,
+            50,
+            14 );
         this.add( button );
-        setBackgroundImg("DungeonBackground1.gif", 800, 600);
-        this.getContentPane().add( ( makeLabel(background) ) );
-        button.addActionListener((new ActionListener()
+        setBackgroundImg( "DungeonBackground1.gif", 800, 600 );
+        this.getContentPane().add( ( makeLabel( background ) ) );
+        button.addActionListener( ( new ActionListener()
         {
 
             @Override
             public void actionPerformed( ActionEvent e )
             {
-                setVisible(false);
+                setVisible( false );
                 Launcher.getGame().startGame();
             }
-            
-        }));
+
+        } ) );
         this.pack();
     }
 
+
+    /**
+     * Creates and returns the menu label given the background image and also
+     * displays the title of the game.
+     * 
+     * @param backgroundImg
+     *            - the background image in which the the label will contain
+     * @return JLabel label object that consists of the given background image
+     */
     @Override
-    protected JLabel makeLabel(ImageIcon backgroundImg)
+    protected JLabel makeLabel( ImageIcon backgroundImg )
     {
-        JLabel label = new JLabel(backgroundImg) 
+        JLabel label = new JLabel( backgroundImg )
         {
             public void paintComponent( Graphics g )
             {
                 super.paintComponent( g );
-                Images.drawText( g, "Dungeon Crawler", 400, 75, true, Color.WHITE, Images.loadFont( 60 ) );
+                Images.drawText( g,
+                    "Dungeon Crawler",
+                    400,
+                    75,
+                    true,
+                    Color.WHITE,
+                    Images.loadFont( 60 ) );
             }
         };
         return label;
     }
-    
 
 }
